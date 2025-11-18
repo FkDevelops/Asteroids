32ea17f8df0da5adb2f966804a21a886ddff7e47
FkDevelops <kfakhar226@gmail.com>
2025-11-17 23:05:17 -0500

    C: moved some of the program inside main() and added a 60 fps limiter

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-
-
 def main():
+    pygame.init()
+    #set screen size
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #Clock object to keep track of time
+    clock = pygame.time.Clock()
+    #delta time
+    dt = 0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -22,8 +24,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-    
 
+        #limit to 60 fps and getting delta time in seconds
+        dt = dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()

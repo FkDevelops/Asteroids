17e05de4dd50db97c1132ec83e8f2178df4e12d1
FkDevelops <kfakhar226@gmail.com>
2025-11-20 09:55:20 -0500

    F: Added Asteroid spawning and movement

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
 
 
 def main():
@@ -10,10 +14,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #Clock object to keep track of time
     clock = pygame.time.Clock()
+    #creating groups for draw and updatable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    #group for asteroids
+    asteroids = pygame.sprite.Group()
+
+    #adding objects to groups
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable, )
     #delta time
     dt = 0
-    #create player object
+    #creating objects
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    A_field = AsteroidField()
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -26,18 +41,20 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        #update all objects in this group
+        updatable.update(dt)
 
         #make the screen black
         screen.fill("black")
         
-        #draw the player 
-        player.draw(screen)
+        #draw everything in the group
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
 
         #limit to 60 fps and getting delta time in seconds
-        dt = dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
